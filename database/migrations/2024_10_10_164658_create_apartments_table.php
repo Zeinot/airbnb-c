4ea307083a5b9e6ef56c42687154e4a36b42c6ff
Zeinot <60817247+Zeinot@users.noreply.github.com>
2024-10-10 17:48:57 +0100

    added apartments table model and migration

---
+++ database/migrations/2024_10_10_164658_create_apartments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            $table->string("type");
+            $table->string("city");
+            $table->integer("price_per_day");
+//            need to add table booking later
+//            $table->foreignId("booking_id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('apartments');
+    }
+};
