b1175134df6b337180a04c42dbda69e4c88f91a2
Zeinot <zeinotgaming@gmail.com>
2024-10-10 20:04:53 +0100

    making apartments admin table page

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -2,6 +2,11 @@
 @section("content")
     <div
         class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-96 mb-4"
-    ></div>
+    >
+        <button type="button" class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">Create</button>
+
+
+
+    </div>
 
 @endsection
