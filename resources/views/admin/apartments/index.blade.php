a022107358800a0e59e81c2772448972dc8bfbb1
Zeinot <zeinotgaming@gmail.com>
2024-10-11 00:10:29 +0100

    added editor to create page

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -1,8 +1,8 @@
 @extends("layouts.custom.admin")
 @section("content")
-@dump(\App\Models\Apartment::all())
+{{--   @dump(\App\Models\Apartment::all())   --}}
     <div
-        class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 mb-4"
+        class="rounded-lg border-gray-300 dark:border-gray-600 mb-4"
     >
         <div class="w-full p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
         <a href="{{ route('apartments.create') }}"><button type="button" class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">Create</button></a>
