968a37b1a06264dbcf6e294d766c9b7fb989ebb8
Zeinot <zeinotgaming@gmail.com>
2024-10-12 22:52:28 +0100

    format + change ids and names of fields

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [ListingController::class, 'index'])->name('home');
 
 Route::get('/apartments', [ApartmentController::class, "index"])->name("apartments.index");
+Route::post('/apartments', [ApartmentController::class, "store"])->name("apartments.store");
 Route::get('/admin', [ApartmentController::class, "admin_index"])->name("apartments.admin_index");
 Route::get('/admin/apartments/create', [ApartmentController::class, "create"])->name("apartments.create");
 
