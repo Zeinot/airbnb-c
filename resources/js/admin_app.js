3a985d584524030e85e5e7723c9cd0d898d26af5
Zeinot <zeinotgaming@gmail.com>
2024-10-13 01:20:21 +0100

    format + change ids and names of fields

--- resources/js/admin_app.js
+++ resources/js/admin_app.js
@@ -8,23 +8,48 @@ if (document.getElementById("filter-table") && typeof simpleDatatables.DataTable
                 return table
             }
             const tHead = table.childNodes[0]
+            // console.log(`${tHead.childNodes[0].childNodes[0].childNodes[0].childNodes[0].data}`.includes(`Actions`) )
+            // console.log(tHead.childNodes[0].childNodes[0].childNodes[0].childNodes[0].nodeName === "SPAN")
             const filterHeaders = {
                 nodeName: "TR",
                 attributes: {
                     class: "search-filtering-row"
                 },
                 childNodes: tHead.childNodes[0].childNodes.map(
-                    (_th, index) => ({nodeName: "TH",
-                        childNodes: [
-                            {
-                                nodeName: "INPUT",
-                                attributes: {
-                                    class: "datatable-input",
-                                    type: "search",
-                                    "data-columns": "[" + index + "]"
+                    (_th, index) => {
+                        // console.log(_th.childNodes[0].childNodes[0].data !== "Actions")
+                        if (tHead.childNodes[0].childNodes[0].childNodes[0].childNodes[0].nodeName !== "SPAN")
+                        return {
+                            nodeName: "TH",
+                                childNodes
+                        :
+                            [
+                                {
+                                    nodeName: "INPUT",
+                                    attributes: {
+                                        class: "datatable-input",
+                                        type: "search",
+                                        "data-columns": "[" + index + "]"
+                                    }
                                 }
-                            }
-                        ]})
+                            ]
+                        }
+                        else return {
+                            nodeName: "TH",
+                            childNodes
+                                :
+                                [
+                                    {
+                                        nodeName: "INPUT",
+                                        attributes: {
+                                            class: "hidden",
+                                            type: "search",
+                                            "data-columns": "[" + index + "]"
+                                        }
+                                    }
+                                ]
+                        }
+                    }
                 )
             }
             tHead.childNodes.push(filterHeaders)
