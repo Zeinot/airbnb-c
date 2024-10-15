dc0596008f6e6eca8ebc0d358edf89a86eb1df38
benmouhabdel <161528151+benmouhabdel@users.noreply.github.com>
2024-10-15 23:28:05 +0200

    ssssssssssss

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -2,22 +2,36 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 class Reservation extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['user_id', 'listing_id', 'start_date', 'end_date'];
+    protected $fillable = [
+        'apartment_id',
+        'user_id',
+        'start_date',
+        'end_date',
+        'status',
+    ];
 
+    /**
+     * Relation avec le modèle Apartment.
+     * Un appartement peut avoir plusieurs réservations.
+     */
+    public function apartment()
+    {
+        return $this->belongsTo(Apartment::class);
+    }
+
+    /**
+     * Relation avec le modèle User.
+     * Un utilisateur peut effectuer plusieurs réservations.
+     */
     public function user()
     {
         return $this->belongsTo(User::class);
-    }
-
-    public function listing()
-    {
-        return $this->belongsTo(Listing::class);
     }
 }
