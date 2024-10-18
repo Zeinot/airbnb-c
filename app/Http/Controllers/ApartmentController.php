85876e43b0c1cc2ee3bb496eaa41e7dcf2a2bfd4
Zeinot <zeinotgaming@gmail.com>
2024-10-18 15:01:37 +0100

    sS

--- app/Http/Controllers/ApartmentController.php
+++ app/Http/Controllers/ApartmentController.php
@@ -38,13 +38,13 @@ class ApartmentController extends Controller
      */
     public function store(Request $request)
     {
-        dump(request()->all());
+        dd(request()->all());
         $request->validate([
             "title" => "required|min:25|max:200",
             "type" => "required|in:Daily,Weekly,Monthly,Yearly",
             "city" => "required|min:3|max:50",
             "address" => "required|min:10|max:200",
-            "description" => "required|min:10|max:20000",
+            "description" => "required|min:10|max:5000",
             "price" => "required|min:0.01",
             "images" => "required",
             'images.*' => 'image|mimes:jpg,jpeg,png,webp,svg|max:20048'
@@ -104,7 +104,7 @@ class ApartmentController extends Controller
             "type" => "required|in:Daily,Weekly,Monthly,Yearly",
             "city" => "required|min:3|max:50",
             "address" => "required|min:10|max:200",
-            "description" => "required|min:10|max:20000",
+            "description" => "required|min:10|max:5000",
             "price" => "required|min:0.01",
             "images" => "required",
             'images.*' => 'image|mimes:jpg,jpeg,png,webp,svg|max:20048'
