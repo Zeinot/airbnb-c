9f90fdec0cf7f3a595ad4c1e77c98e858736b407
Zeinot <zeinotgaming@gmail.com>
2024-10-18 15:31:02 +0100

    s

--- resources/views/admin/apartments/edit.blade.php
+++ resources/views/admin/apartments/edit.blade.php
@@ -88,24 +88,20 @@
                 <div>
                     <label for="description"
                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
-                    {{-- @dump(old('description'), $apartment->description) --}}
                     <textarea id="description" name="description" rows="4"
                         class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         placeholder="Description...">@if(old('description')){{ old('description') }}@else{{ $apartment->description }}@endif</textarea>
                 </div>
-
                 <div class="flex gap-3">
                     <a href="{{ route('apartments.admin_index') }}"
                         class="w-fit text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                         Cancel
                     </a>
-
                     <div id="submit">
                         <button type="submit" onclick="handleSubmit()"
                             class="w-fit text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                             Update Apartment
                         </button>
-
                     </div>
                 </div>
                 <br />
