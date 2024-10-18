ecaf250a5e8fd98e169ed4c9dbdb7d599dc74483
Zeinot <zeinotgaming@gmail.com>
2024-10-18 18:45:03 +0100

    s

--- resources/views/apartments/show.blade.php
+++ resources/views/apartments/show.blade.php
@@ -255,11 +255,11 @@
                             {{$apartment->description}}
                         </p>
 
-                        <p class="text-gray-500 dark:text-gray-400">
+{{--                        <p class="text-gray-500 dark:text-gray-400">
                             Two Thunderbolt USB 4 ports and up to two USB 3 ports. Ultrafast
                             Wi-Fi 6 and Bluetooth 5.0 wireless. Color matched Magic Mouse with
                             Magic Keyboard or Magic Keyboard with Touch ID.
-                        </p>
+                        </p>--}}
                     </div>
                 </div>
             </div>
