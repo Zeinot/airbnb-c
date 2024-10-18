d760c9c3ec217d138e3940167bdf0f4d4ca2c572
Zeinot <zeinotgaming@gmail.com>
2024-10-18 18:38:37 +0100

    s

--- resources/views/apartments/show.blade.php
+++ resources/views/apartments/show.blade.php
@@ -56,13 +56,14 @@
                 <div class="lg:grid lg:grid-cols-2 lg:gap-8 xl:gap-16">
                     <div class="max-w-md lg:max-w-lg mx-auto">
                         <div id="product-1-tab-content">
+                            @foreach([1,2,3,4] as $i)
                             <div class="hidden p-4 rounded-lg bg-white dark:bg-gray-900" id="product-1-image-1"
                                  role="tabpanel" aria-labelledby="product-1-image-1-tab">
                                 <img class="w-full mx-auto dark:hidden"
                                      src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front.svg" alt=""/>
 
                             </div>
-
+                            @endforeach
                         </div>
 
                         <ul class="grid grid-cols-4 gap-4 mt-8" id="product-1-tab"
@@ -71,7 +72,7 @@
                             data-tabs-inactive-classes="border-transparent hover:border-gray-200 dark:hover:dark:border-gray-700 dark:border-transparent"
                             role="tablist">
 
-
+           @foreach([1,2,3,4] as $i)
                             <li class="me-2" role="presentation">
                                 <button
                                     class="h-20 w-20 overflow-hidden border-2 rounded-lg sm:h-20 sm:w-20 md:h-24 md:w-24 p-2 cursor-pointer mx-auto"
@@ -86,7 +87,7 @@
                                 </button>
                             </li>
 
-
+              @endforeach
                         </ul>
 
                     </div>
