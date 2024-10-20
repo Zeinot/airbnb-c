efd13612334efe98e4d4101e7f4dad30d96f0c70
Zeinot <zeinotgaming@gmail.com>
2024-10-20 04:53:43 +0100

    s

--- resources/views/layouts/custom/home.blade.php
+++ resources/views/layouts/custom/home.blade.php
@@ -210,7 +210,7 @@
                                     Profile</a></li>
                             <li><a href="{{route("apartments.admin_index")}}" title=""
                                    class="inline-flex w-full items-center gap-2 rounded-md px-3 py-2 text-sm hover:bg-gray-100 dark:hover:bg-gray-600">
-                                    My Apartments </a></li>
+                                    Apartments </a></li>
                             {{--                            <li><a href="#" title=""
                                                                class="inline-flex w-full items-center gap-2 rounded-md px-3 py-2 text-sm hover:bg-gray-100 dark:hover:bg-gray-600">
                                                                 Settings </a></li>
