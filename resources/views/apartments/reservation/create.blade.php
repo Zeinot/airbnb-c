8f875353daea6c571d9128ff28c4bee42b45320e
Zeinot <zeinotgaming@gmail.com>
2024-10-28 10:53:34 +0100

    s

--- resources/views/apartments/reservation/create.blade.php
+++ resources/views/apartments/reservation/create.blade.php
@@ -1,8 +1,8 @@
 @extends("layouts.custom.home")
 @section("content")
 
-    hello from reservation page
-    @dump($apartment)
+{{--    hello from reservation page--}}
+{{--    @dump($apartment)--}}
     <section class="bg-gray-50 dark:bg-gray-900">
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
             <div
