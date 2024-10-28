cd8dd17bb969b64c2457ba9ae1f1825a776e3df9
Zeinot <zeinotgaming@gmail.com>
2024-10-28 10:15:42 +0100

    s

--- resources/views/apartments/reservation/create.blade.php
+++ resources/views/apartments/reservation/create.blade.php
@@ -11,16 +11,17 @@
                     <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                         Contact
                     </h1>
-                    <form class="space-y-4 md:space-y-6" action="#">
+                    <form method="POST" class="space-y-4 md:space-y-6" action="{{route("apartments.reservation.send", $apartment)}}">
+                        @csrf
                         <div>
                             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email
                                 *</label>
                             <input type="email" name="email" id="email"
-                                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                                    placeholder="name@company.com" required="">
                         </div>
                         <div>
-                            <label for="phone-input"
+                            <label for="phone"
                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone number
                                 *</label>
                             <div class="relative">
@@ -32,22 +33,23 @@
                                             d="M18 13.446a3.02 3.02 0 0 0-.946-1.985l-1.4-1.4a3.054 3.054 0 0 0-4.218 0l-.7.7a.983.983 0 0 1-1.39 0l-2.1-2.1a.983.983 0 0 1 0-1.389l.7-.7a2.98 2.98 0 0 0 0-4.217l-1.4-1.4a2.824 2.824 0 0 0-4.218 0c-3.619 3.619-3 8.229 1.752 12.979C6.785 16.639 9.45 18 11.912 18a7.175 7.175 0 0 0 5.139-2.325A2.9 2.9 0 0 0 18 13.446Z"/>
                                     </svg>
                                 </div>
-                                <input type="text" id="phone-input" aria-describedby="helper-text-explanation"
-                                       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                       pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="123-456-7890" required/>
+                                <input type="text" id="phone" name="phone" aria-describedby="helper-text-explanation"
+                                       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+{{--                                       pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" --}}
+                                       placeholder="123-456-7890" required/>
                             </div>
                         </div>
                         <div>
                             <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">More
                                 Info</label>
                             <textarea id="message" rows="4"
-                                      class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                      class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                                       placeholder="Write your thoughts here..."></textarea>
 
                         </div>
                         <button type="submit"
                                 class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                            Create an account
+                            Send
                         </button>
                     </form>
                 </div>
