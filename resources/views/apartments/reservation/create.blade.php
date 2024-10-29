ebeea0ef679a1b92d194cb45943a90062a9f6df2
Zeinot <zeinotgaming@gmail.com>
2024-10-29 09:25:47 +0100

    s

--- resources/views/apartments/reservation/create.blade.php
+++ resources/views/apartments/reservation/create.blade.php
@@ -66,7 +66,7 @@ disabled
                                     class="cursor-wait h-[40] w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                                 <div role="status" class="flex justify-center">
                                     <svg aria-hidden="true"
-                                         class="w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                                         class="w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-primary-600"
                                          viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path
                                             d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
